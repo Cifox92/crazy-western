2cd4e25b888fb7333fa3d015ba12b8e76f33d790
David <drgomez92@gmail.com>
2020-06-19 09:57:38 +0200

    Versión 1.1. ESTABLE. Se añaden tres modos de dificultad al inicio del juego.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,30 @@
 window.onload = () => {
     document.getElementById("gameArea").classList.toggle('hidden')
-    document.getElementById("start-game").onclick = () => {
+    document.getElementById("easy").onclick = () => {
         Game.init()
-        document.getElementById("gameArea").classList.remove('hidden')
-        document.getElementById("gameArea").classList.toggle('appearGame')
-        document.getElementById("intro").classList.toggle('hidden')
+        startGame()
     }
+
+    document.getElementById("normal").onclick = () => {
+        Game.init()
+        Game.player.playerLifes = 10
+        Game.framesFinal = 7000
+        startGame()
+    }
+
+    document.getElementById("hard").onclick = () => {
+        Game.init()
+        Game.player.playerLifes = 8
+        Game.framesFinal = 10000
+        Game.alien.alienLifes = 3
+        Game.martians.martianLifes = 5
+        Game.finalBoss.finalBossLifes = 40
+        startGame()
+    }
+}
+
+function startGame() {
+    document.getElementById("gameArea").classList.remove('hidden')
+    document.getElementById("gameArea").classList.toggle('appearGame')
+    document.getElementById("intro").classList.toggle('hidden')
 }
